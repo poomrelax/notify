966a1bd673b8e49c244e58dc20b0313319f27d4d
homeworkrelax <homeworkrelax@gmail.com>
2025-07-12 12:05:36 +0700

    update12/7/2568

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: 'all',
-    host: true,
-    port: 5173
-  }
+  // server: {
+  //   // allowedHosts: 'all',
+  //   host: '0.0.0.0',
+  //   port: 5173
+  // }
 })
